fea5ed6299c81062c57d89b610a4ccd4ac167f46
BlueMystic <jhollman.chacon@gmail.com>
2022-01-25 17:36:50 -0300

    Add files via upload

---
+++ source/Traductor/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Traductor
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Load File:";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.Location = new System.Drawing.Point(12, 41);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.textBox1.Size = new System.Drawing.Size(450, 423);
+			this.textBox1.TabIndex = 1;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(341, 12);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(121, 23);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Recover Translation";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(470, 465);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Elite - Dangerous (CLIENT)";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button2;
+	}
+}
+
---
+++ source/Traductor/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using EDHM_UI_mk2;
+
+namespace Traductor
+{
+	public partial class Form1 : Form
+	{
+		private ui_setting Settings = new ui_setting();
+
+		private string _path = string.Empty;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog OFDialog = new OpenFileDialog()
+			{
+				Filter = "JSON Data|*.json",
+				FilterIndex = 0,
+				DefaultExt = "json",
+				AddExtension = true,
+				CheckPathExists = true,
+				CheckFileExists = true,
+				InitialDirectory = @"D:\Soft TEST\ED UI mk2\EDHM_UI_mk2\Data"
+			};
+
+			if (OFDialog.ShowDialog() == DialogResult.OK)
+			{
+				this.Settings = Util.DeSerialize_FromJSON<ui_setting>(OFDialog.FileName);
+				if (this.Settings != null)
+				{
+					int i = 0;
+					this._path = OFDialog.FileName;
+					StringBuilder _Texto = new StringBuilder();
+					foreach (var _Grupo in this.Settings.ui_groups)
+					{
+						_Texto.AppendLine(_Grupo.Title); i++;
+						foreach (var _Elemento in _Grupo.Elements)
+						{
+							_Texto.AppendLine(string.Format("{0}|{1}|{2}", _Elemento.Category, _Elemento.Title, 
+								_Elemento.Description.Replace("\r\n", "<p>")));
+							i++;
+						}
+					}
+					this.textBox1.Text = _Texto.ToString() + "|" + i;
+				}
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if (this.textBox1.Text != string.Empty)
+			{
+				string[] Lineas = this.textBox1.Text.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+				if (Lineas != null && Lineas.Length > 0 && this.Settings != null)
+				{
+					int i = 0;
+					foreach (var _Grupo in this.Settings.ui_groups)
+					{
+						string[] Palabras;
+						_Grupo.Title = Lineas[i]; i++;
+						foreach (var _Elemento in _Grupo.Elements)
+						{
+							Palabras = Lineas[i].Split(new char[] { '|' });
+
+							_Elemento.Category = Palabras[0].Trim(); 
+							_Elemento.Title = Palabras[1].Trim(); 
+							_Elemento.Description = Palabras[2].Replace("<p>","\r\n").Trim();
+
+							i++;
+						}
+					}
+
+					SaveFileDialog SFDialog = new SaveFileDialog()
+					{
+						Filter = "JSON Data|*.json",
+						FilterIndex = 0,
+						DefaultExt = "json",
+						AddExtension = true,
+						CheckPathExists = true,
+						OverwritePrompt = true,
+						FileName = this._path,
+						InitialDirectory = System.IO.Path.GetDirectoryName(this._path)
+					};
+					if (SFDialog.ShowDialog() == DialogResult.OK)
+					{
+						Util.Serialize_ToJSON(SFDialog.FileName, this.Settings);
+						MessageBox.Show("Saved", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+					}
+				}
+			}
+		}
+	}
+}
