777012cde481995436f19d0839ade21195e0c4f3
BlueMystic <jhollman.chacon@gmail.com>
2022-01-25 17:41:04 -0300

    Add files via upload

---
+++ source/elite-dangerous-64/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace elite_dangerous_64
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// listBox1
+			// 
+			this.listBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(0, 33);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(445, 199);
+			this.listBox1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(128, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Elite Dangerous Simulator";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(445, 232);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.listBox1);
+			this.KeyPreview = true;
+			this.Name = "Form1";
+			this.Text = "Elite - Dangerous (CLIENT)";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+			this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ source/elite-dangerous-64/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace elite_dangerous_64
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			//KeyPress puede detectar las siguientes Teclas:
+			//a - z, A - Z; CTRL; Punctuation marks; ENTER; Number keys, both across the top of the keyboard and on the numeric keypad.
+			if (e.KeyChar != (char)Keys.ControlKey)
+			{
+				this.listBox1.Items.Add(string.Format("KeyPress: {0}", e.KeyChar.ToString()));
+			}
+		}
+
+		private void Form1_KeyDown(object sender, KeyEventArgs e)
+		{
+			string Modifiers = e.Modifiers.ToString();
+			this.listBox1.Items.Add(string.Format("KeyDown: {0}{1}",
+				Modifiers != "None" ? Modifiers + " + " : string.Empty,
+				e.KeyCode.ToString()));
+
+			// CTRL+G:
+			if (e.KeyCode == Keys.G && (e.Control))
+			{
+				//GotoLine();
+			}
+		}
+	}
+}
