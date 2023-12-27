cdc5ba4883be2e5bca225d4335235b7a50eda1ef
BlueMystic <jhollman.chacon@gmail.com>
2023-12-27 14:04:11 -0300

    v2.2.15

--- source/elite-dangerous-64/Form1.Designer.cs
+++ source/elite-dangerous-64/Form1.Designer.cs
@@ -31,17 +31,21 @@
 			this.label1 = new System.Windows.Forms.Label();
 			this.lblStatus = new System.Windows.Forms.Label();
 			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.cmdEmbark = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// label1
 			// 
-			this.label1.AutoSize = true;
-			this.label1.BackColor = System.Drawing.Color.White;
-			this.label1.Location = new System.Drawing.Point(149, 4);
+			this.label1.BackColor = System.Drawing.Color.Transparent;
+			this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.label1.Location = new System.Drawing.Point(0, 0);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(128, 13);
+			this.label1.Size = new System.Drawing.Size(1083, 20);
 			this.label1.TabIndex = 1;
 			this.label1.Text = "Elite Dangerous Simulator";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			this.label1.DoubleClick += new System.EventHandler(this.label1_DoubleClick);
 			// 
 			// lblStatus
 			// 
@@ -56,6 +60,7 @@
 			// 
 			// comboBox1
 			// 
+			this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
 			this.comboBox1.BackColor = System.Drawing.Color.Black;
 			this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.comboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
@@ -71,12 +76,22 @@
             "federation_corvette|INDOMITABLE|RW-01",
             "sidewinder|sidewinder|ZA-06S",
             "type9_military|TALOS|ZA-07T"});
-			this.comboBox1.Location = new System.Drawing.Point(955, 29);
+			this.comboBox1.Location = new System.Drawing.Point(950, 29);
 			this.comboBox1.Name = "comboBox1";
 			this.comboBox1.Size = new System.Drawing.Size(121, 21);
 			this.comboBox1.TabIndex = 3;
 			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
 			this.comboBox1.DropDownClosed += new System.EventHandler(this.comboBox1_DropDownClosed);
+			// 
+			// cmdEmbark
+			// 
+			this.cmdEmbark.Location = new System.Drawing.Point(996, 56);
+			this.cmdEmbark.Name = "cmdEmbark";
+			this.cmdEmbark.Size = new System.Drawing.Size(75, 23);
+			this.cmdEmbark.TabIndex = 4;
+			this.cmdEmbark.Text = "Embark";
+			this.cmdEmbark.UseVisualStyleBackColor = true;
+			this.cmdEmbark.Click += new System.EventHandler(this.cmdEmbark_Click);
 			// 
 			// Form1
 			// 
@@ -84,7 +99,8 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackgroundImage = global::elite_dangerous_64.Properties.Resources.FDL_Clean;
 			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-			this.ClientSize = new System.Drawing.Size(1088, 631);
+			this.ClientSize = new System.Drawing.Size(1083, 635);
+			this.Controls.Add(this.cmdEmbark);
 			this.Controls.Add(this.comboBox1);
 			this.Controls.Add(this.lblStatus);
 			this.Controls.Add(this.label1);
@@ -103,6 +119,7 @@
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Label lblStatus;
 		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button cmdEmbark;
 	}
 }
 
--- source/elite-dangerous-64/Form1.cs
+++ source/elite-dangerous-64/Form1.cs
@@ -1,17 +1,35 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace elite_dangerous_64
 {
-	public partial class Form1 : Form
+	public partial class Form1 : Form, IMessageFilter
 	{
+		public const int WM_NCLBUTTONDOWN = 0xA1;
+		public const int HT_CAPTION = 0x2;
+		public const int WM_LBUTTONDOWN = 0x0201;
+
+		[DllImportAttribute("user32.dll")]
+		public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+		[DllImportAttribute("user32.dll")]
+		public static extern bool ReleaseCapture();
+
+		private HashSet<Control> controlsToMove = new HashSet<Control>();
+
 		public Form1()
 		{
 			InitializeComponent();
+
+			Application.AddMessageFilter(this);
+
+			controlsToMove.Add(this);
+			controlsToMove.Add(this.label1);//Add whatever controls here you want to move the form when it is clicked and dragged
 		}
 
 		private void Form1_Load(object sender, EventArgs e)
@@ -134,6 +152,74 @@ namespace elite_dangerous_64
 					tw.Close();
 
 					this.lblStatus.Text = string.Format("Journal - Ship Loadout: {0}", _Ship.ed_short);
+				}
+			}
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+			
+		}
+		private void label1_DoubleClick(object sender, EventArgs e)
+		{
+			OpenFileDialog OFDialog = new OpenFileDialog()
+			{
+				Filter = "Archivos de Imagenes|*.jpg;*.bmp;*.gif;*.png|Todos los archivos|*.*",
+				FilterIndex = 0,
+				DefaultExt = "png",
+				AddExtension = true,
+				CheckPathExists = true,
+				CheckFileExists = true,
+				InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+			};
+
+			if (OFDialog.ShowDialog() == DialogResult.OK)
+			{
+				this.BackgroundImage = Image.FromFile(OFDialog.FileName);
+				this.FormBorderStyle = FormBorderStyle.SizableToolWindow;
+				this.ControlBox = false;
+				this.Text = String.Empty;
+
+				label1.BackColor = Color.Transparent;
+				label1.ForeColor = Color.White;
+				label1.Text = "ALT+F4 para Salir.";
+				label1.TextAlign = ContentAlignment.MiddleRight;
+			}
+		}
+
+		//Para mover la ventana sin titulo
+		public bool PreFilterMessage(ref Message m)
+		{
+			if (m.Msg == WM_LBUTTONDOWN &&
+				 controlsToMove.Contains(Control.FromHandle(m.HWnd)))
+			{
+				ReleaseCapture();
+				SendMessage(this.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+				return true;
+			}
+			return false;
+		}
+
+		private void cmdEmbark_Click(object sender, EventArgs e)
+		{
+			string EDJournalDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), @"Saved Games\Frontier Developments\Elite Dangerous");
+			if (Directory.Exists(EDJournalDir))
+			{
+				DirectoryInfo di = new DirectoryInfo(EDJournalDir);
+
+				//Busca el Archivo de Log mas reciente:
+				FileInfo JournalFile = di.GetFiles("Journal.*.log")
+							.OrderByDescending(f => f.LastWriteTime).First();
+
+				if (JournalFile != null)
+				{
+					ed_ship _Ship = this.comboBox1.SelectedItem as ed_ship;
+
+					TextWriter tw = new StreamWriter(JournalFile.FullName, append: true);
+					tw.WriteLine("{\"timestamp\":\"2023 - 11 - 27T08: 23:44Z\",\"event\":\"Embark\",\"SRV\":false,\"Taxi\":false,\"Multicrew\":false,\"ID\":72,\"StarSystem\":\"Bleae Thua WH-G b38-5\",\"SystemAddress\":11653454440777,\"Body\":\"Bleae Thua WH-G b38-5 A\",\"BodyID\":2,\"OnStation\":false,\"OnPlanet\":false} " );
+					tw.Close();
+
+					this.lblStatus.Text = string.Format("Journal - Embark: {0}", _Ship.ed_short);
 				}
 			}
 		}
